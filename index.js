1fd2878a0efb44fc8aadba5c4a6fe3ba19088307
Phil Hartley <philh725@gmail.com>
2018-10-04 21:00:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {'hi': 6}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, {`key`: value} )
+  return Object.assign(object, {key: value} )
 }
 
 console.log(updateObjectWithKeyAndValue(recipes, 'test', 2))
